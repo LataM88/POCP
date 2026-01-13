b2f5ff988eaba4692c5f7f42c4f14e50dfa9bc5d
Michał <latamichal88@gmail.com>
2026-01-13 12:34:22 +0100

    jako admin

--- main.py
+++ main.py
@@ -7,4 +7,16 @@ def main():
     
 
 if __name__ == "__main__":
-    main()
+    import ctypes, sys
+
+    def is_admin():
+        try:
+            return ctypes.windll.shell32.IsUserAnAdmin()
+        except:
+            return False
+
+    if is_admin():
+        main()
+    else:
+        # Re-run the program with admin rights
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
